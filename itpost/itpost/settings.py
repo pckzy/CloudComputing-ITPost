76f3a1213abb8c9524e1ac4bf6882a3f3b435952
“pckzy” <sarun.debsirin@gmail.com>
2025-10-29 12:53:10 +0700

    cloud computing

--- itpost/itpost/settings.py
+++ itpost/itpost/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.1.25.70', '127.0.0.1']
 
 
 # Application definition
